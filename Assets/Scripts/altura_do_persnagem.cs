9dc27f538245376b7c01017dc82f2876c4b62457
Miho <mihojp1@hotmail.com>
2019-05-21 23:38:19 -0300

    :tada: Initial commit

---
+++ Assets/Scripts/altura_do_persnagem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class altura_do_persnagem : MonoBehaviour {
+
+	public Transform personagem;
+	Text texto_da_interface;
+
+	// Use this for initialization
+	void Start () {
+		texto_da_interface = GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log (personagem.position.y);
+		texto_da_interface.text = Mathf.RoundToInt(personagem.position.y).ToString();
+	}
+}
